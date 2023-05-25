c1908944a7a7e942bc5923fb5df168bd458dcb74
ycjung@idrsoft.com <ychung@idrsoft.com>
2023-05-25 14:19:00 +0900

    2023.05.25 ycjung spring boot 프로젝트 최초 커밋

---
+++ PeriodicDatabaseFeeder/src/main/java/com/ycjung/schedule/PeriodicDatabaseFeederApplication.java
@@ -0,0 +1,13 @@
+package com.ycjung.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriodicDatabaseFeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeriodicDatabaseFeederApplication.class, args);
+	}
+
+}
